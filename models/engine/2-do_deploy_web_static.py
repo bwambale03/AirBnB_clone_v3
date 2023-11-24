2c3b7aba23c9d7e721b9e2c97cecb152a158b088
Edwin Bwambale <bwambaleedwin03@gmail.com>
2023-11-24 01:58:53 -0800

    edited ip addr

--- models/engine/2-do_deploy_web_static.py
+++ models/engine/2-do_deploy_web_static.py
@@ -2,7 +2,7 @@
 import os.path
 from fabric.api import env, put, run
 
-env.hosts = ["54.157.152.10", "54.157.160.52"]
+env.hosts = ["54.157.145.62", "54.157.138.182"]
 
 
 def do_deploy(archive_path):
